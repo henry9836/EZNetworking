84815063703bd3d408d77e9ed46c0428954ba138
henry9836 <henry983615@gmail.com>
2020-01-06 14:37:05 +1300

    Init

---
+++ Assets/Scripts/NetworkTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkTest : MonoBehaviour
+{
+
+    const int port = 13371;
+
+    public void StartServer()
+    {
+        Debug.Log("Server Mode");
+    }
+
+    public void StartClient()
+    {
+        Debug.Log("Client Mode");
+    }
+
+    private void Start()
+    {
+        
+    }
+}
