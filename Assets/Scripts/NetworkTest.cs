581e0bc63075971551f7a526466e20c4a6ceeff8
Henry <henry983615@gmail.com>
2020-01-06 16:37:40 +1300

    Can now host as well but it is very rough
    needs to talk back and forth as well as handling disconnects

--- Assets/Scripts/NetworkTest.cs
+++ Assets/Scripts/NetworkTest.cs
@@ -6,115 +6,81 @@ using System.Text;
 using System.Collections.Generic;
 using UnityEngine;
 
-/*
-// A C# program for Client 
-namespace Client
-{
-
-    class Program
-    {
-
-        // Main Method 
-        static void Main(string[] args)
-        {
-            ExecuteClient();
-        }
-
-        // ExecuteClient() Method 
-        static void ExecuteClient()
-        {
-
-            try
-            {
-
-                // Establish the remote endpoint 
-                // for the socket. This example 
-                // uses port 11111 on the local 
-                // computer. 
-                IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
-                IPAddress ipAddr = ipHost.AddressList[0];
-                IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
-
-                // Creation TCP/IP Socket using 
-                // Socket Class Costructor 
-                Socket sender = new Socket(ipAddr.AddressFamily,
-                        SocketType.Stream, ProtocolType.Tcp);
-
-                try
-                {
-
-                    // Connect Socket to the remote 
-                    // endpoint using method Connect() 
-                    sender.Connect(localEndPoint);
-
-                    // We print EndPoint information 
-                    // that we are connected 
-                    Console.WriteLine("Socket connected to -> {0} ",
-                                sender.RemoteEndPoint.ToString());
-
-                    // Creation of messagge that 
-                    // we will send to Server 
-                    byte[] messageSent = Encoding.ASCII.GetBytes("Test Client<EOF>");
-                    int byteSent = sender.Send(messageSent);
-
-                    // Data buffer 
-                    byte[] messageReceived = new byte[1024];
-
-                    // We receive the messagge using 
-                    // the method Receive(). This 
-                    // method returns number of bytes 
-                    // received, that we'll use to 
-                    // convert them to string 
-                    int byteRecv = sender.Receive(messageReceived);
-                    Console.WriteLine("Message from Server -> {0}",
-                        Encoding.ASCII.GetString(messageReceived,
-                                                    0, byteRecv));
-
-                    // Close Socket using 
-                    // the method Close() 
-                    sender.Shutdown(SocketShutdown.Both);
-                    sender.Close();
-                }
-
-                // Manage of Socket's Exceptions 
-                catch (ArgumentNullException ane)
-                {
-
-                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
-                }
-
-                catch (SocketException se)
-                {
-
-                    Console.WriteLine("SocketException : {0}", se.ToString());
-                }
-
-                catch (Exception e)
-                {
-                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
-                }
-            }
-
-            catch (Exception e)
-            {
-
-                Console.WriteLine(e.ToString());
-            }
-        }
-    }
-}
-
-
- */
-
 public class NetworkTest : MonoBehaviour
 {
 
     const int port = 13371;
+    public string targetIP = "127.0.0.1";
 
     public void StartServer()
     {
         Debug.Log("Server Mode");
+        // Establish the local endpoint  
+        // for the socket. Dns.GetHostName 
+        // returns the name of the host  
+        // running the application. 
+        IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+        IPAddress ipAddr = ipHost.AddressList[0];
+        ipAddr = IPAddress.Parse(targetIP);
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddr, port);
+
+        // Creation TCP/IP Socket using  
+        // Socket Class Costructor 
+        Socket listener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+        // Using Bind() method we associate a 
+        // network address to the Server Socket 
+        // All client that will connect to this  
+        // Server Socket must know this network 
+        // Address 
+        listener.Bind(localEndPoint);
+
+        // Using Listen() method we create  
+        // the Client list that will want 
+        // to connect to Server 
+        listener.Listen(10);
+
+        while (true)
+        {
+
+            Debug.Log("Waiting connection ... ");
+
+            // Suspend while waiting for 
+            // incoming connection Using  
+            // Accept() method the server  
+            // will accept connection of client 
+            Socket clientSocket = listener.Accept();
+
+            // Data buffer 
+            byte[] bytes = new Byte[1024];
+            string data = null;
+
+            while (true)
+            {
+
+                int numByte = clientSocket.Receive(bytes);
+
+                data += Encoding.ASCII.GetString(bytes,
+                                           0, numByte);
+
+                if (data.IndexOf("<EOF>") > -1)
+                    break;
+            }
+
+            Console.WriteLine("Text received -> {0} ", data);
+            byte[] message = Encoding.ASCII.GetBytes("Test Server");
+
+            // Send a message to Client  
+            // using Send() method 
+            clientSocket.Send(message);
+
+            // Close client Socket using the 
+            // Close() method. After closing, 
+            // we can use the closed Socket  
+            // for a new Client Connection 
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+        }
     }
 
     public void StartClient()
@@ -125,7 +91,7 @@ public class NetworkTest : MonoBehaviour
         // for the socket.
         IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
         IPAddress ipAddr = host.AddressList[0];
-        ipAddr = IPAddress.Parse("127.0.0.1");
+        ipAddr = IPAddress.Parse(targetIP);
         IPEndPoint localEndPoint = new IPEndPoint(ipAddr, port);
 
 
@@ -167,8 +133,4 @@ public class NetworkTest : MonoBehaviour
 
     }
 
-    private void Start()
-    {
-        
-    }
 }
