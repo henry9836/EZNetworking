0fa57ccef2a9e5df540a8d12ccdd283918cfcd20
henry9836 <henry983615@gmail.com>
2020-02-07 13:42:27 +1300

    Rigidbody Movement

---
+++ Assets/Scripts/RespawnBall.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnBall : MonoBehaviour
+{
+    public float respawnDistance = 10.0f;
+    public float speed = 1000.0f;
+
+    private Vector3 respawnPoint = Vector3.zero;
+    private Rigidbody rb = null;
+
+    private void Start()
+    {
+        respawnPoint = GameObject.FindGameObjectWithTag("Respawn").transform.position;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Atlas.isClient)
+        {
+            if (Vector3.Distance(respawnPoint, transform.position) > respawnDistance)
+            {
+                rb.velocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+                transform.position = respawnPoint;
+            }
+            rb.AddForce(Vector3.right * speed * Time.deltaTime);
+        }
+    }
+}
